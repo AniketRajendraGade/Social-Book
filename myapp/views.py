edea1f549687c8ecfadb04cf7da80e5c4c3fcea8
AniketRajendraGade <aniketgade04@gmail.com>
2024-06-22 23:07:00 +0530

    mmm

--- myapp/views.py
+++ myapp/views.py
@@ -130,9 +130,9 @@ def bulkdata(request):
 
 
 
-# @csrf_exempt  
-# def bulkdata(request):
-#     start_time = time.time()
+@csrf_exempt  
+def bulkdata(request):
+    start_time = time.time()
     excel_file_path = r'C:\Users\results-20240429-050336-1714367055093.csv'
 
     df = pd.read_csv(excel_file_path, encoding='utf-8')
