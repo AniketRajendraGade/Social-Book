b4559d862735978bf7da0d293d4bccb32592ec12
AniketRajendraGade <aniketgade04@gmail.com>
2024-09-23 13:43:41 +0530

    vfgr

--- myapp/views.py
+++ myapp/views.py
@@ -46,8 +46,8 @@ def login_view(request):
 @login_required
 def home(request):
     user = request.user
-    # all_books = Books.objects.filter(visibility=True)
-    # return render(request, 'home.html', {'user': user, 'all_books': all_books})
+    all_books = Books.objects.filter(visibility=True)
+    return render(request, 'home.html', {'user': user, 'all_books': all_books})
 
 @login_required()
 def upload_book(request):
